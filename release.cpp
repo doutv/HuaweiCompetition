da0e49dad45011c582eeaa2399b56007bcc5670e
Yongjin Huang <1435267417@qq.com>
2020-04-21 22:40:37 +0800

    update IO fread+fwrite 常数优化

--- release.cpp
+++ release.cpp
@@ -11,15 +11,13 @@
 #include <unordered_set>
 using namespace std;
 
-char input_path[] = "/data/test_data.txt";
-char output_path[] = "/projects/student/result.txt";
+string input_path = "/data/test_data.txt";
+string output_path = "/projects/student/result.txt";
 
 const int INF = 280005;
 typedef long long ll;
 
-// 出边
 vector<int> GUV[INF];
-// 入边
 vector<int> GVU[INF];
 int edge_size;
 
@@ -36,50 +34,82 @@ ans_t ans[4000005];
 int u_arr[INF];
 int v_arr[INF];
 
-void read_data()
+namespace IO
 {
-    freopen(input_path, "r", stdin);
+const int MAXSIZE = 1 << 20;
+char buf[MAXSIZE], *p1, *p2;
+#define gc()                                                                 \
+    (p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, MAXSIZE, stdin), p1 == p2) \
+         ? EOF                                                               \
+         : *p1++)
+inline int rd()
+{
+    int x = 0;
+    char c = gc();
+    if (c == EOF)
+        return -1;
+    while (!isdigit(c))
+    {
+        c = gc();
+    }
+    while (isdigit(c))
+        x = x * 10 + (c ^ 48), c = gc();
+    return x;
+}
+char pbuf[1 << 20], *pp = pbuf;
+inline void push(const char &c)
+{
+    if (pp - pbuf == 1 << 20)
+        fwrite(pbuf, 1, 1 << 20, stdout), pp = pbuf;
+    *pp++ = c;
+}
+inline void write(int x)
+{
+    static int sta[35];
+    int top = 0;
+    do
+    {
+        sta[top++] = x % 10, x /= 10;
+    } while (x);
+    while (top)
+        push(sta[--top] + '0');
+}
+} // namespace IO
+
+inline void read_data()
+{
+    freopen(input_path.c_str(), "r", stdin);
     int u, v;
     int ch;
+    register int i;
     while (1)
     {
-        ch = getchar();
-        if (ch == EOF)
+        u = IO::rd();
+        if (u == -1)
             break;
-        u = 0;
-        while ('0' <= ch && ch <= '9')
-        {
-            u = (u << 3) + (u << 1) - '0' + ch;
-            ch = getchar();
-        }
-        v = 0;
-        while ('0' <= (ch = getchar()) && ch <= '9')
-            v = (v << 3) + (v << 1) - '0' + ch;
-        ch = getchar();
-        while ('0' <= ch && ch <= '9')
-            ch = getchar();
+        v = IO::rd();
+        IO::rd();
         node[++node_size] = u;
         node[++node_size] = v;
         ++edge_size;
         u_arr[edge_size] = u;
         v_arr[edge_size] = v;
     }
-    //离散化
     sort(node + 1, node + node_size + 1);
     node_size = unique(node + 1, node + node_size + 1) - node - 1;
-    for (int i = 1; i <= node_size; i++)
+    for (i = 1; i <= node_size; i++)
     {
         node_hashmap[node[i]] = i;
     }
-    for (int i = 1; i <= edge_size; i++)
+    for (i = 1; i <= edge_size; i++)
     {
-        int u = node_hashmap[u_arr[i]];
-        int v = node_hashmap[v_arr[i]];
+        u = node_hashmap[u_arr[i]];
+        v = node_hashmap[v_arr[i]];
         GUV[u].push_back(v_arr[i]);
         GVU[v].push_back(u_arr[i]);
     }
 }
-bool cmp(ans_t &x, ans_t &y)
+inline bool cmp(ans_t &x, ans_t &y)
 {
     int now = 0;
     while (x[now] == y[now])
@@ -90,9 +120,11 @@ void flag_traverse_dfs(int u, int depth, int target)
 {
     if (depth <= 2)
     {
-        for (int i = 0; i < GUV[u].size(); i++)
+        register int i;
+        int v;
+        for (i = 0; i < GUV[u].size(); i++)
         {
-            int v = node_hashmap[GUV[u][i]];
+            v = node_hashmap[GUV[u][i]];
             if (!visited[v] && GUV[u][i] > target)
             {
                 visited[v] = 1;
@@ -107,9 +139,11 @@ void flag_reverse_dfs(int u, int depth, int target)
 {
     if (depth <= 2)
     {
-        for (int i = 0; i < GVU[u].size(); i++)
+        register int i;
+        int v;
+        for (i = 0; i < GVU[u].size(); i++)
         {
-            int v = node_hashmap[GVU[u][i]];
+            v = node_hashmap[GVU[u][i]];
             if (!visited[v] && GVU[u][i] > target)
             {
                 visited[v] = 1;
@@ -122,9 +156,11 @@ void flag_reverse_dfs(int u, int depth, int target)
 }
 void dfs(int u, int depth, ans_t &path, int target)
 {
-    for (int i = 0; i < GUV[u].size(); i++)
+    register int i;
+    int v;
+    for (i = 0; i < GUV[u].size(); i++)
     {
-        int v = node_hashmap[GUV[u][i]];
+        v = node_hashmap[GUV[u][i]];
         if (GUV[u][i] < target)
             continue;
         if (flag[v] == -2 && visited[v] == 0)
@@ -149,58 +185,58 @@ void dfs(int u, int depth, ans_t &path, int target)
         }
     }
 }
-void work()
+inline void work()
 {
     memset(flag, -1, node_size + 5);
     ans_t path;
-    for (int i = 1; i <= node_size; i++)
+    int v, target;
+    register int i, j;
+    for (i = 1; i <= node_size; i++)
     {
-        int target = node[i];
+        target = node[i];
         flag_traverse_dfs(i, 0, target);
         flag_reverse_dfs(i, 0, target);
-        for (int j = 0; j < GVU[i].size(); j++)
+        for (j = 0; j < GVU[i].size(); j++)
         {
-            int v = node_hashmap[GVU[i][j]];
-            // 特殊标记倒走一步的点
+            v = node_hashmap[GVU[i][j]];
             flag[v] = -2;
         }
         path[1] = target;
         dfs(i, 1, path, target);
-        for (int j = 0; j < GVU[i].size(); j++)
+        for (j = 0; j < GVU[i].size(); j++)
         {
-            int v = node_hashmap[GVU[i][j]];
+            v = node_hashmap[GVU[i][j]];
             flag[v] = target;
         }
     }
 }
-void out(int x)
+inline void out(int x)
 {
     if (x > 9)
         out(x / 10);
     putchar(x % 10 + '0');
 }
 
-void output_data()
+inline void output_data()
 {
-    freopen(output_path, "w", stdout);
+    register int i, j;
+    freopen(output_path.c_str(), "w", stdout);
     sort(ans + 1, ans + ans_size + 1, cmp);
     printf("%d\n", ans_size);
-    for (int i = 1; i <= ans_size; i++)
+    for (i = 1; i <= ans_size; i++)
     {
-        int len = ans[i][0];
-        for (int j = 1; j < len; j++)
+        for (j = 1; j < ans[i][0]; j++)
         {
-            out(ans[i][j]);
+            IO::write(ans[i][j]);
             putchar(',');
         }
-        out(ans[i][len]);
+        IO::write(ans[i][ans[i][0]]);
         putchar('\n');
     }
     return;
 }
 int main()
 {
-    auto time_start = chrono::steady_clock::now();
     read_data();
     work();
     output_data();
