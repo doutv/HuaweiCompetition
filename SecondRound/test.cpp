0fb4fb09d28d9e1d900a531a8e5c0d8933a030e3
Yongjin Huang <1435267417@qq.com>
2020-05-09 11:10:56 +0800

    ans array pointer FAIL

--- SecondRound/test.cpp
+++ SecondRound/test.cpp
@@ -6,12 +6,10 @@ const int count = 32;
 
 int main()
 {
-    int ans[2][3] = {{1, 2, 3}, {4, 5, 6}};
-    int *ptr[1] = {*ans};
-    int x = **ans;
-    cout << x << endl;
-    cout << *(ptr[0] + 1) << endl;
-    ++*(ptr[0] + 1);
-    cout << *(ptr[0] + 3) << endl;
+    int ans2[2] = {1, 2};
+    int ans3[2] = {12, 13};
+    int *ans[2] = {ans2, ans3};
+    int *nowans = ans[0];
+    cout << ++*nowans << endl;
     return 0;
 }
